10830c65d5162e059876c86572aeb2d0b09621de
Nikita <kucenko.nikita@gmail.com>
2023-02-19 17:14:26 +0700

    Add entitites

---
+++ client/src/components/list/OptionsItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <li class="options-item">{{ item.id }}</li>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "@vue/runtime-core";
+
+export default defineComponent({
+  name: "OptionsItem",
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/_base.scss";
+</style>
---
+++ client/src/components/list/OptionsList.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul class="options-list">
+
+  </ul>
+</template>
+
+<script lang="ts">
+
+
+export default {
+  name: "OptionsList"
+}
+</script>
+
+<style scoped>
+
+</style>
