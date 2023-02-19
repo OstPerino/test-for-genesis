84319d2e8f4535d1bbcf73ba5e96a95f50008661
Nikita <kucenko.nikita@gmail.com>
2023-02-19 15:50:19 +0700

    Add styles for dropdown

--- client/src/components/UI/CustomDropdown.vue
+++ client/src/components/UI/CustomDropdown.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="custom-dropdown">
+  <div class="dropdown">
     <button @click="toggleDropdown">{{ selectedOption.label }}</button>
     <ul v-show="state.isDropdownOpen">
       <li v-for="option in options" :key="option.value" @click="onChangeHandler(option)">
@@ -53,12 +53,93 @@ export default defineComponent({
 <style lang="scss" scoped>
 @import "@/styles/_base.scss";
 
-button {
-  border: 1px solid $black;
+.dropdown {
+  position: relative;
+  display: inline-block;
+  margin: 10px;
+  font-family: Roboto, sans-serif;
+  font-size: 14px;
 }
 
-li {
-  border: 1px solid $black;
+.dropdown button {
+  position: relative;
+  background-color: #fff;
+  border: 1px solid #a5a5a5;
+  border-radius: 4px;
+  color: #454545;
+  padding: 10px;
+  padding-right: 30px;
+  min-width: 200px;
+  text-align: left;
+  font-weight: 500;
   cursor: pointer;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+}
+
+.dropdown button:focus {
+  outline: none;
+  box-shadow: 0 0 0 2px rgba(0, 99, 255, 0.2);
+}
+
+.dropdown button:before {
+  content: '';
+  position: absolute;
+  top: 50%;
+  right: 10px;
+  width: 0;
+  height: 0;
+  margin-top: -3px;
+  border-top: 6px solid #999;
+  border-right: 6px solid transparent;
+  border-left: 6px solid transparent;
+  pointer-events: none;
+}
+
+.dropdown ul {
+  position: absolute;
+  top: 100%;
+  left: 0;
+  z-index: 1;
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  background-color: #fff;
+  border: 1px solid #a5a5a5;
+  border-top: none;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  width: 100%;
+  max-height: 200px;
+  overflow-y: auto;
+  overflow-x: hidden;
+}
+
+.dropdown li {
+  padding: 10px;
+  cursor: pointer;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  font-weight: 400;
+}
+
+.dropdown li:hover {
+  background-color: #eee;
+}
+
+.dropdown li.selected {
+  background-color: #d1d1d1;
+}
+
+.dropdown li.disabled {
+  color: #c7c7c7;
+  cursor: default;
+}
+
+.dropdown li.divider {
+  height: 1px;
+  margin: 5px 0;
+  background-color: #a5a5a5;
 }
 </style>
